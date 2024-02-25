614d6895e9fc348896abf6fe74685779205042bf
naila <nailahamaw@gmail.com>
2024-02-25 17:58:22 +0300

    updates files content

--- apps/taskmodule/urls.py
+++ apps/taskmodule/urls.py
@@ -2,5 +2,7 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('task',views.task),
+    path('tasks/<int:tid>', views.tasks)
     
 ]
--- apps/taskmodule/views.py
+++ apps/taskmodule/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     # study the request
     return render(request, 'taskmodule/index.html') # rendering the template
+
+def task(request):
+    # study the request
+    return render(request, 'taskmodule/task.html') # rendering the template
+
+def tasks(request, tid):
+    # study the request
+    task1 = {'id':123, 'priority':'high', 'sec': 'study'}
+    task2 = {'id':456, 'priority':'low', 'sec': 'shop'}
+    
+    targetT =None
+    
+    if task1['id'] == tid : targetT=task1
+    if task2['id'] == tid : targetT=task2
+    
+    context= {'task':targetT}
+    
+    if targetT == None : return redirect('/')
+    
+    return render(request, 'taskmodule/tasks.html', context) # rendering the template
